4300dfc8603bfcf372bf9d862cd0a64be85c9201
fcosic1 <fcosic1@etf.unsa.ba>
2023-08-23 09:22:02 +0200

    add product in domain

--- src/main/java/ba/unsa/etf/rpr/domain/Product.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Product.java
@@ -1,4 +1,54 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Product {
+public class Product implements Idable{
+
+    private int id;
+    private String description;
+    private double price;
+    private byte[] picture;
+
+    public Product(){
+
+    }
+
+    public Product(double price, String description){
+        this.price = price;
+        this.description = description;
+    }
+    @Override
+    public void setId(int id) {
+
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public byte[] getPicture(){
+        return picture;
+    }
+    public void setPicture(byte[] picture){
+        this.picture = picture;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product p = (Product) o;
+        return id == p.id;
+    }
 }
