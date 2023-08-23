688ca82aa031a2d271928a9761a81cd91c7c5be9
fcosic1 <fcosic1@etf.unsa.ba>
2023-08-23 10:12:31 +0200

    implement abstractdao

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.dao;
 
-public class UserDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.ProjectException;
+
+import java.util.List;
+
+public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao{
+
+    private static UserDaoSQLImpl instance = null;
+    private UserDaoSQLImpl(){
+        super("User");
+    }
+
 }
