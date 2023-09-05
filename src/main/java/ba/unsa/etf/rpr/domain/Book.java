6dddcc517baf66fb6b531c791c75f6fff39bdd27
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-05 21:51:43 +0200

    izmjene

--- src/main/java/ba/unsa/etf/rpr/domain/Book.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Book.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Book {
+import java.util.Date;
+
+public class Book implements Idable{
+
+    private int id;
+    private String bookTitle;
+    private Date ageOfBook;
+    private String author;
+    private double price;
+    private String bookType;
+
+
+    public Book(int book_id, String bookTitle, Date ageOfBook, String author, double price, String bookType){
+        this.id = book_id;
+        this.bookTitle = bookTitle;
+        this.ageOfBook = ageOfBook;
+        this.author = author;
+        this.price = price;
+        this.bookType = bookType;
+    }
+    @Override
+    public void setId(int id) {
+
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
 }
