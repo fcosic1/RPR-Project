0e4757b60bc777adf16fc2fb39c9677b91a96aef
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-05 21:58:28 +0200

    book

--- src/main/java/ba/unsa/etf/rpr/domain/Book.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Book.java
@@ -12,14 +12,35 @@ public class Book implements Idable{
     private String bookType;
 
 
-    public Book(int book_id, String bookTitle, Date ageOfBook, String author, double price, String bookType){
+    public Book(int book_id, String bookTitle, Date ageOfBook, String author, double price, String bookTypeID){
         this.id = book_id;
         this.bookTitle = bookTitle;
         this.ageOfBook = ageOfBook;
         this.author = author;
         this.price = price;
-        this.bookType = bookType;
+        this.bookType = bookTypeID;
     }
+
+    public Book(){
+        this.id = 0;
+        this.bookTitle = "";
+        this.ageOfBook = new Date();
+        this.author = "";
+        this.price = 0;
+        this.bookType = "";
+    }
+
+
+    public String getBookTitle() { return bookTitle;}
+    public void setBookTitle(String bookTitle){ this.bookTitle = bookTitle;}
+    public Date getAgeOfBook() {return ageOfBook;}
+    public void setAgeOfBook(Date ageOfBook){this.ageOfBook = ageOfBook;}
+    public String getAuthor() {return author;}
+    public void setAuthor(String author) {this.author = author;}
+    public double getPrice(){return price;}
+    public void setPrice(double price){this.price = price;}
+    public String getBookType() {return bookType;}
+    public void setBookType(String bookTypeId) {this.bookType = bookTypeId;}
     @Override
     public void setId(int id) {
 
@@ -29,4 +50,6 @@ public class Book implements Idable{
     public int getId() {
         return 0;
     }
+
+
 }
