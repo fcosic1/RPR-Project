30899cba171206bfd8ea4504cfbba404c6b36633
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-06 23:32:43 +0200

    purchase dao izmjena

--- src/main/java/ba/unsa/etf/rpr/dao/PurchaseDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PurchaseDao.java
@@ -5,10 +5,11 @@ import ba.unsa.etf.rpr.domain.Purchase;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.ProjectException;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PurchaseDao extends Dao<Purchase> {
     List<Purchase> getByBook(Book book) throws ProjectException;
     List<Purchase> getByUser(User user) throws ProjectException;
-
+    List <Purchase> getByDate(Date date) throws ProjectException;
 }
