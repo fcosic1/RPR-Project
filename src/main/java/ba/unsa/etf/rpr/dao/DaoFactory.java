7431dd253db1bcb2ab7c5f0c9e7f192a35bae2e6
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-06 22:53:30 +0200

    mali problem sa daofactory

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Book;
+
 public class DaoFactory {
+    private static final UserDao userDao = new UserDaoSQLImplementation();
+    private static final BookDao bookDao = new BookDaoSQLImplementation();
+
+    private static final PurchaseDao purchaseDao = new PurchaseDaoSQLImplementation();
+    private DaoFactory(){}
+    public static UserDao userDao(){
+        return userDao;
+    }
+    public static BookDao bookDao(){
+        return bookDao;
+    }
+    public static PurchaseDao purchaseDao(){
+        return purchaseDao;
+    }
 }
