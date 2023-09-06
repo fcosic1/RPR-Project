9fed812d1086e318c3b436a5cd2d5b80511ba27f
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-06 23:45:44 +0200

    add PurchaseDaoSQLImplementation

--- src/main/java/ba/unsa/etf/rpr/dao/PurchaseDaoSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PurchaseDaoSQLImplementation.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.dao;
 
-public class PurchaseDaoSQLImplementation {
+import ba.unsa.etf.rpr.domain.Purchase;
+
+public class PurchaseDaoSQLImplementation extends AbstractDao<Purchase> implements PurchaseDao {
+    private static PurchaseDaoSQLImplementation instance = null;
+
+    public static PurchaseDaoSQLImplementation getInstance(){
+        if(instance == null) instance = new PurchaseDaoSQLImplementation();
+        return instance;
+    }
+    public static void removeInstance(){
+        if(instance != null) instance = null;
+    }
 }
