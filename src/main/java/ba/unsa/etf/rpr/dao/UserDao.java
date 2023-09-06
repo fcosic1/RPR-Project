35fb3f2ea2673544b6efe3860b80bf7fb614b9ce
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-06 15:02:56 +0200

    izmjena

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -5,7 +5,7 @@ import ba.unsa.etf.rpr.exceptions.ProjectException;
 
 import java.util.List;
 
-public interface UserDao {
+public interface UserDao extends Dao<User> {
     List<User> searchByFirstName(String firstName) throws ProjectException;
     List<User> searchByLastName(String lastName) throws ProjectException;
     User searchByUsername(String username) throws ProjectException;
