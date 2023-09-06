d53c14451f0b6df6f422e5ea0ff9732ecdc56690
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-06 23:51:09 +0200

    getbybook implement

--- src/main/java/ba/unsa/etf/rpr/dao/PurchaseDaoSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PurchaseDaoSQLImplementation.java
@@ -1,6 +1,14 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Book;
 import ba.unsa.etf.rpr.domain.Purchase;
+import ba.unsa.etf.rpr.exceptions.ProjectException;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PurchaseDaoSQLImplementation extends AbstractDao<Purchase> implements PurchaseDao {
     private static PurchaseDaoSQLImplementation instance = null;
@@ -11,5 +19,24 @@ public class PurchaseDaoSQLImplementation extends AbstractDao<Purchase> implemen
     }
     public static void removeInstance(){
         if(instance != null) instance = null;
+    }
+    public PurchaseDaoSQLImplementation(){
+        super("Purchase");
+    }
+    @Override
+    public List<Purchase> getByBook(Book book) throws ProjectException {
+        List<Purchase> list = new ArrayList<>();
+        try {
+            PreparedStatement s = getConnection().prepareStatement("select * from Purchase where book = ?");
+            s.setInt(1, book.getId());
+            ResultSet rs = s.executeQuery();
+            while (rs.next()) {
+                list.add(row2object(rs));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            throw new ProjectException(e.getMessage(),e);
+        }
+        return list;
     }
 }
