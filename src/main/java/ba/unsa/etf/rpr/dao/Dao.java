9cdd27042f72250c6e157e911dbc0c42813bbca6
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-06 14:54:45 +0200

    Dao implementation

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -2,5 +2,13 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
 
+import java.util.List;
+
 public interface Dao<type> {
+
+    type getById(int id);
+    type add(type item);
+    type update(type item);
+    void delete(int id);
+    List<type> getAll();
 }
