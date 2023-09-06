8e1952aa1246302b3f3521ffed3ca1394b0569b0
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-06 14:59:20 +0200

    userdao interface implementation

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.ProjectException;
+
+import java.util.List;
+
 public interface UserDao {
+    List<User> searchByFirstName(String firstName) throws ProjectException;
+    List<User> searchByLastName(String lastName) throws ProjectException;
+    User searchByUsername(String username) throws ProjectException;
 }
