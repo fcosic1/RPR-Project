c91c5e5a9c5c9e5b6a8f7dac61f73d543c77332a
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-10 00:04:34 +0200

    add getall in PurchaseManager

--- src/main/java/ba/unsa/etf/rpr/business/PurchaseManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/PurchaseManager.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Purchase;
+import ba.unsa.etf.rpr.exceptions.ProjectException;
+
+import java.util.List;
+
 public class PurchaseManager {
+    public List<Purchase> getAll() throws ProjectException {
+        return DaoFactory.purchaseDao().getAll();
+    }
 }
