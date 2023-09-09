6ec8f042d327ba95a4be718052e09011cf98eb7f
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-09 17:17:00 +0200

    add fields in singupcont

--- src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.UserManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class SignupController {
+    public Button buttonRegister;
+    public TextField fieldName;
+    public TextField fieldLastName;
+    public TextField fieldEmail;
+    public TextField fieldUsername;
+    public PasswordField fieldPassword;
+    private final UserManager userManager = new UserManager();
+
 }
