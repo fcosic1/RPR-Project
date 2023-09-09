e578f0ff145dff30e4b641651cc3ac44900faeb6
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-10 00:13:31 +0200

    add getters and setters

--- src/main/java/ba/unsa/etf/rpr/business/PurchaseTableView.java
+++ src/main/java/ba/unsa/etf/rpr/business/PurchaseTableView.java
@@ -17,4 +17,47 @@ public class PurchaseTableView {
         this.price=price;
         this.purchase_date=date;
     }
+    public PurchaseTableView() {
+
+    }
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBookType() {
+        return bookType;
+    }
+
+    public void setBookType(String bookType) {
+        this.bookType = bookType;
+    }
+
+    public Date getPurchase_date() {
+        return purchase_date;
+    }
+
+    public void setPurchase_date(Date purchase_date) {
+        this.purchase_date = purchase_date;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
