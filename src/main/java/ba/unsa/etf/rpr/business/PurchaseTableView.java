7bc4591e7e45c180e7e82bf9d9ea9dfffd12cffc
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-10 00:11:06 +0200

    add consturctor

--- src/main/java/ba/unsa/etf/rpr/business/PurchaseTableView.java
+++ src/main/java/ba/unsa/etf/rpr/business/PurchaseTableView.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.business;
 
+import java.util.Date;
+
 public class PurchaseTableView {
+
+    private String bookTitle;
+    private String author;
+    private String bookType;
+    private Date purchase_date;
+    private double price;
+
+    public PurchaseTableView(String bookTitle, String author, String bookType, double price, Date date) {
+        this.bookTitle=bookTitle;
+        this.author=author;
+        this.bookType=bookType;
+        this.price=price;
+        this.purchase_date=date;
+    }
 }
