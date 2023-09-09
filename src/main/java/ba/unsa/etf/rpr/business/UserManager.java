a053be7429dc466bac58cb673ea83fac236400a1
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-09 17:03:42 +0200

    add checklogin

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.ProjectException;
 
 public class UserManager {
@@ -8,5 +9,14 @@ public class UserManager {
             throw new ProjectException("Username field empty");
         else if(username.trim().length()<5 || username.trim().length()>15)
             throw new ProjectException("Username needs to be between 5 and 15 characters long!");
+    }
+
+    public void checkLogIn(String username, String password){
+        checkUsername(username);
+        //checkPassword(password);
+        UserManager userManager=new UserManager();
+        //User user = userManager.searchByUsername(username);
+        //if(user == null) throw new ProjectException("No user is registered with given username!");
+        //if(!user.getPassword().equals(password)) throw new ProjectException("Password incorrect!");
     }
 }
