73dc7d138fd8d56e38fba009cd182a582fa524d8
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-09 23:50:28 +0200

    add getAll in BookManager

--- src/main/java/ba/unsa/etf/rpr/business/BookManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/BookManager.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.exceptions.ProjectException;
+
+import java.util.List;
+
 public class BookManager {
+    public List<Book> getAll() throws ProjectException {
+        return DaoFactory.bookDao().getAll();
+    }
+
 }
