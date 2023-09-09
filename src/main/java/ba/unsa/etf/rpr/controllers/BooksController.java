a6e0f3cd72c5b2c8ad46d7993dd68a0ac9061310
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-09 23:31:10 +0200

    izmjene

--- src/main/java/ba/unsa/etf/rpr/controllers/BooksController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/BooksController.java
@@ -1,4 +1,50 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class BooksController {
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.domain.Purchase;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class BooksController implements Initializable {
+    @FXML
+    public TableColumn column_title;
+    @FXML
+    public TableColumn column_author;
+    @FXML
+    public TableColumn column_bookType;
+    @FXML
+    public TableColumn column_ageOfBook;
+    @FXML
+    public TableColumn column_price;
+    public TextField searchTextField;
+    public Button buttonBuy;
+    @FXML
+    private TableView user_table;
+    @FXML
+    private ObservableList<Book> bookList = FXCollections.observableArrayList();
+    private final BookManager bookManager = new BookManager();
+    private final PurchaseManager purchaseManager = new PurchaseManager();
+    private final UserManager userManager = new UserManager();
+
+
 }
