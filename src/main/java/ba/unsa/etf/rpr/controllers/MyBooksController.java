f6ed1654fee3cb94596ef95a1a5a2d523ee81d36
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-10 00:36:28 +0200

    izmjena

--- src/main/java/ba/unsa/etf/rpr/controllers/MyBooksController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MyBooksController.java
@@ -8,8 +8,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MyBooksController implements Initializable {
@@ -30,6 +32,20 @@ public class MyBooksController implements Initializable {
     private final PurchaseTableViewManager purchaseTableViewManager=new PurchaseTableViewManager();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        List<PurchaseTableView> list;
+        if(LoginController.getUsername() != null)
+            list = purchaseTableViewManager.getMyBooks(LoginController.getUsername());
+        else list = purchaseTableViewManager.getMyBooks(SignupController.username);
+        for(int i=0;i<list.size();i++){
+            bookList.add(new PurchaseTableView(list.get(i).getBookTitle(),list.get(i).getAuthor(),list.get(i).getBookType(),list.get(i).getPrice(),list.get(i).getPurchase_date()));
+        }
+        column_title.setCellValueFactory(new PropertyValueFactory<>("bookTitle"));
+        column_author.setCellValueFactory(new PropertyValueFactory<>("author"));
+        column_bookType.setCellValueFactory(new PropertyValueFactory<>("bookType"));
+        column_date.setCellValueFactory(new PropertyValueFactory<>("purchase_date"));
+        column_price.setCellValueFactory(new PropertyValueFactory<>("price"));
+        myBooksTable.setItems(bookList);
     }
+
 }
+
