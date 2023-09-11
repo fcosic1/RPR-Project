c9c2a071c0dad6cf68c860fa0a382197fe4b8ff5
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-11 22:19:06 +0200

    izmjena

--- src/main/java/ba/unsa/etf/rpr/exceptions/ProjectException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/ProjectException.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.exceptions;
 
 import java.sql.SQLException;
 
+/** Kreiran izuzetak ProjectException */
 public class ProjectException extends RuntimeException {
     public ProjectException(String message, SQLException reason) {
         super(message);
