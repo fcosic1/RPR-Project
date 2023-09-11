bd56e1a759c63f7b7cfe74cfd9b6da378e15b738
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-11 17:10:29 +0200

    add managers in test

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -1,6 +1,11 @@
 package ba.unsa.etf.rpr;
 
 
+import ba.unsa.etf.rpr.business.BookManager;
+import ba.unsa.etf.rpr.business.PurchaseManager;
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.domain.User;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -10,6 +15,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class AppTest 
 {
+
+    BookManager bookManager = new BookManager();
+    UserManager userManager = new UserManager();
+    PurchaseManager purchaseManager = new PurchaseManager();
     /**
      * Rigorous Test :-)
      */
@@ -18,4 +27,6 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+
 }
