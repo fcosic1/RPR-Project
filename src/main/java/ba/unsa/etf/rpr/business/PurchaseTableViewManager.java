a9f937d2c845dc55982623d8902701d81bde371a
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-12 00:22:28 +0200

    izmjena

--- src/main/java/ba/unsa/etf/rpr/business/PurchaseTableViewManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/PurchaseTableViewManager.java
@@ -4,7 +4,7 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.exceptions.ProjectException;
 
 import java.util.List;
-
+/** Business logic layer for TableView in MyBooks section*/
 public class PurchaseTableViewManager {
     public List<PurchaseTableView> getMyBooks(String username) throws ProjectException {
         return DaoFactory.purchaseDao().getMyBooks(username);
