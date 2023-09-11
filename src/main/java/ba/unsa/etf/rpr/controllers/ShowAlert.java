dabde361e969f20684e7632fa204e489fd39616d
fcosic1 <fcosic1@etf.unsa.ba>
2023-09-11 22:33:55 +0200

    izmjena

--- src/main/java/ba/unsa/etf/rpr/controllers/ShowAlert.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ShowAlert.java
@@ -2,7 +2,9 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.scene.control.Alert;
 
+/**class that display an alert on screen*/
 public class ShowAlert {
+    /**method which displays an alert on screen*/
     public static void showAlert(String title, String header, String content){
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
